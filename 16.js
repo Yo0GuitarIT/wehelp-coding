8577832b7c6d53d1a7cdef439a5ca63901e35fbd
Mac <mac@Yo0-Mac.local>
2024-01-10 22:37:03 +0800

    add solution

---
+++ 16.js
@@ -0,0 +1,12 @@
+/*
+    @param n:{Integer}
+    @return :{Boolean}
+*/
+function checkEven(n){
+  return (n%2 === 0);
+}
+
+
+console.log(checkEven(0));
+console.log(checkEven(-7));
+console.log(checkEven(102));
---
+++ 18.js
@@ -0,0 +1,11 @@
+function formatFloat(n){
+  return n.toFixed(2);
+}
+
+
+console.log(formatFloat(0));
+console.log(formatFloat(5.4658));
+console.log(formatFloat(-3.123));    
+console.log(formatFloat(100.1));
+
+
